ee7349ed639fda963731fe1ef8c4ea255eecef58
Marco-FI-SEI <mmc90@hotmail.co.uk>
2020-06-12 13:14:00 +0100

    📦 NEW: implement checking off tasks and helpers to clean views

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,19 @@
 module ItemsHelper
+  def li_class_for_item(item)
+    "completed" if item.complete?
+  end
+
+  def li_for_item(item)
+    content_tag :li, item, class: li_class_for_item(item) do
+      yield
+    end
+  end
+
+  def form_for_item_status(item)
+    form_with model: [item.list, item], local: true do |f|
+      hidden_field_tag("item[status]", "0") + 
+      check_box_tag("item[status]", "1", item.complete?, class: "toggle") +
+      label_tag("item[status]", "#{item.description}")
+    end
+  end
 end
--- app/javascript/packs/lists.js
+++ app/javascript/packs/lists.js
@@ -6,17 +6,3 @@ checkboxes.forEach(checkbox => {
     e.preventDefault();
   });
 });
-
-// Parameters: {
-//   "authenticity_token"=>"RrhLDNrceDIcnAFYTXWzwcoaSHQZ6ykeUSqJFSepIFcPMsTPi4kq0yIT3kLgOigKg7Vf3yZ5gq/ZP1BPj5agvQ==",
-//   "item"=>{"1"=>"1"},
-//   "list_id"=>"2",
-//   "id"=>"1"
-// }
-
-// Parameters: {
-//   "authenticity_token"=>"hlHCb/YRg8VhPfl1qitCnr3AJmtevYsEBqQKwNaeGgTP202sp0TRJF+yJm8HZNlV9G8xwGEvILWOsdOafqGa7g==",
-//   "item"=>{"status"=>"0"},
-//   "list_id"=>"5",
-//   "id"=>"4"
-// }
